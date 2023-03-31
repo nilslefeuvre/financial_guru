2e12194f39cd14a78131c9d527a342aa8572624b
nilslefeuvre <129529228+nilslefeuvre@users.noreply.github.com>
2023-03-31 17:57:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ input = test[['Date','Adj Close']]
 input.columns = ['ds','y']
 input['ds'] = pd.to_datetime(input['ds']).dt.strftime("%Y-%m-%d")
 
-if option = 'Prophet':
+if option == 'Prophet':
   # modelling
   m = Prophet(daily_seasonality=True)
   m.fit(input)
@@ -55,24 +55,22 @@ if option = 'Prophet':
   # Plot 
   fig = px.line(test, x="Date",y="Adj Close")
   fig.data[0].name="Prix observé"
-
   fig.data[0].line.color = "#3A49F9"
   fig.update_traces(showlegend=True)
-
   fig.add_scatter(x=forecast['ds'], y=forecast['yhat'], mode='lines', name='Prix prévisionnel', line=dict(color="#F5241E"))
-
   # fig.update_layout(title_text=f'Predicted {ticker} Stock Prices For Next Year', title_x=0.5)
-
   title = f"Voici le cours prévisionnel de l"+"'"+f"actif {ticker} pour les {nb_jours} prochains jours"
   
   st.markdown(f"<h3 style='text-align: center; color: grey;'>{title}</h3>",unsafe_allow_html=True)
-
   st.plotly_chart(fig)
   
- elif option = 'SARIMAX':
+ elif option == 'SARIMAX':
+  # modelling
   model = sm.tsa.SARIMAX(input, order=(1,1,1), seasonal_order=(2,2,0,12))
   results = model.fit()
   forecast = results.predict(start=end_date, end=(pd.to_datetime(end_date) + relativedelta(days=nb_jours)).strftime("%Y-%m-%d"))
+    
+  # Plot
   fig,(ax1,ax2) = plt.subplots(2,figsize=(18,10))
   forecast.plot(label='Forecasts',ax=ax1,title='SARIMA Forecasting')
   test.plot(label='Actual',ax=ax1)
